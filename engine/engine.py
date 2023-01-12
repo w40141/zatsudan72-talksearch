12de58c9ba566aaaec611fa22104be352aa08c99
w40141 <w.40141@gmail.com>
2023-01-12 23:10:40 +0900

    feat: get_recordes

--- engine/engine.py
+++ engine/engine.py
@@ -6,15 +6,16 @@ import os
 import whisper
 from sudachipy import tokenizer
 from sudachipy import dictionary
+from algoliasearch.search_client import SearchClient
 
 
-class Tmp:
+class Engine:
     def __init__(self):
         load_dotenv()
-        self.rss = os.getenv("PODCAST RSS")
-        self.app_id = os.getenv("ALGOLIA_APP_ID")
-        self.search_key = os.getenv("ALGOLIA_APP_SEARCH_KEY")
-        self.admin_key = os.getenv("ALGOLIA_APP_ADMIN_KEY")
+        self.rss = os.getenv("PODCAST_RSS", "")
+        self.index_name = os.getenv("INDEX_NAME", "")
+        self.app_id = os.getenv("ALGOLIA_APP_ID", "")
+        self.app_key = os.getenv("ALGOLIA_APP_KEY", "")
 
     # 1. RSSからエピソードの取得する
     def get_episode(self) -> Any:
@@ -27,8 +28,9 @@ class Tmp:
         for link in links:
             if link.ref == "enclosure":
                 e = {
+                    "episode_id": entry.id,
                     "title": entry.title,
-                    "number": entry.itunes_episode
+                    "episode_number": entry.itunes_episode
                     if entry.has("itunes_episode")
                     else 0,
                     "media_url": link.ref,
@@ -37,17 +39,22 @@ class Tmp:
             break
         return e
 
-    # 2. 新規エピソードか確認する
-    def check_new_episode(self):
-        pass
+    # 2. エピソードをダウンロードする
+    def download_episodes(self, episodes: dict[str, dict[str, str]]):
+        recodes = self._get_recodes()
+        for key, episode in episodes.values():
+            if key not in recodes:
+                self._download_episode(episode)
 
+    # 2-1. indexを取得する
     def _get_recodes(self):
-        pass
+        # Use an API key with `browse` ACL
+        client = SearchClient.create(self.app_id, self.app_key)
+        index = client.init_index(self.index_name)
+        return list(index.browse_objects({"attributesToRetrieve": ["episode_id"]}))
 
-    # 2-1. 新規エピソードでないのは2へ戻る
-
-    # 2-2. エピソードをダウンロードする
-    def download_episode(self, episode: dict[str, str]):
+    # 2-2. ダウンロードする
+    def _download_episode(self, episode):
         urllib.request.urlretrieve(
             episode["media_url"], self._media_path(episode["number"])
         )
